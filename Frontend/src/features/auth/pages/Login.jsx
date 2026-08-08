5b75c70ecb56d937ab261ee18e89d163c3446aaf
Neeraj <neeraj701776@gmail.com>
2026-08-09 00:26:27 +0530

    Initial commit: backend and frontend setup

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "../auth.form.scss"
+import { useNavigate , Link} from 'react-router'
+
+const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+    };
+    
+  return (
+    
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder='Enter email address' />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder='Enter password' />
+                </div>
+
+                <button className="button primary-button" type="submit">Login</button> 
+
+            </form>    
+
+            <p>Don't have an account? <Link to="/Register">Register</Link></p>           
+
+        </div>
+    </main>
+  )
+}
+
+export default Login
