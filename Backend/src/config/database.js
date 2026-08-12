6bc15652a3ed097b2ccadf4266f0600adea5287f
Neeraj <neeraj701776@gmail.com>
2026-08-12 17:26:21 +0530

    increase mongoose timeouts for serverless cold starts

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -5,11 +5,14 @@ const mongoose = require("mongoose")
 async function connectToDB() {
 
     try {
-        await mongoose.connect(process.env.MONGO_URI)
-
-        console.log("Connected to Database")
-    }
-    catch (err) {
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 20000,
+            socketTimeoutMS: 45000,
+            bufferCommands: true,
+        });
+        mongoose.set("bufferTimeoutMS", 20000);
+        console.log("Connected to Database");
+    } catch (err) {
         console.log(err)
     }
 }
